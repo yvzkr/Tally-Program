edddde82fd5cebf67e157ff7703d10cb7d8c4be9
yvzkr <yavuzkuru3@gmail.com>
2018-05-30 15:02:08 +0300

    fonksiyon yeniden yazıldı ve const kullanıldı

--- Tall-Program.js
+++ Tall-Program.js
@@ -1,24 +1,19 @@
 document.getElementById("tall-program").onclick = function(){
     var girdi = document.getElementById("tall-input").value;
-    tallProgram(girdi);
+    tallSkor(girdi);
 };
 
-
-function tallProgram(girdi){
-    var oyuncular = {a:0,b:0,c:0,d:0,e:0};
+function tallSkor(girdi){
+    const hash = {};
+    const getHash = (l)   => hash[l.toLowerCase()];
+    const setHash = (l,v) => {hash[l.toLowerCase()] += v;}
     
-    for(var i=0; i < girdi.length;i++){
-        
-        switch(girdi[i]){
-                case "a"|| "A": oyuncular.a+=1; break;
-                case "b"|| "B": oyuncular.b+=1; break;
-                case "c"|| "C": oyuncular.c+=1; break;
-                case "d"|| "D": oyuncular.d+=1; break;
-          
-                default: console.log("bulunamadı"); break;
-        } 
-        
-    }
-    console.log(oyuncular);
+    girdi.split('').forEach(harf => {
+        if(!getHash(harf)) hash[harf.toLowerCase()] = 0;
+        if(harf.toUpperCase() === harf) setHash(harf, -1)
+        else setHash(harf, 1);
+    });
+    
+    console.log(Object.entries(hash).map(val => `${val[0]}: ${val[1]}`).join(', '));
+    
 }
-
